5bd9e0f9ded922ed351326adc838118d50a66673
Matthew Schranz <schranz.m@gmail.com>
2014-01-15 13:30:36 -0500

    rework

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,90 +1,37 @@
 /*
- * grunt-html5-lint
- * https://github.com/matt/grunt-html5-lint
+ * grunt-lint5
+ * https://github.com/mozilla/html5-lint
  *
- * Copyright (c) 2013 Matthew Schranz
+ * Copyright (c) 2013 Mozilla
  * Licensed under the MIT license.
  */
 
-"use strict";
+'use strict';
 
-module.exports = function( grunt ) {
+module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON( "package.json" ),
-
     jshint: {
       all: [
-        "Gruntfile.js",
-        "tasks/*.js"
+        'Gruntfile.js',
+        'tasks/*.js'
       ],
       options: {
-        jshintrc: ".jshintrc",
+        jshintrc: '.jshintrc',
       },
-    },
-
-    html5_lint: {
-      dist: {
-        options: {
-          files: [
-            "test/valid/",
-            "test/invalid/"
-          ],
-          ignoreList: [
-            {
-              // Don't warn on valid docs
-              text: "The document is valid HTML5 + ARIA + SVG 1.1 + MathML 2.0 (subject to the utter previewness of this service)."
-            },
-            {
-              text: "Start tag seen without seeing a doctype first. Expected “<!DOCTYPE html>”.",
-              htmlFragment: true
-            },
-            {
-              text: "Element “head” is missing a required instance of child element “title”.",
-              htmlFragment: true
-            },
-            {
-              text: "Bad value “X-UA-Compatible” for attribute “http-equiv” on element “meta”."
-            },
-            {
-              text: "Warning: The character encoding of the document was not declared."
-            },
-            {
-              text: "Attribute “mozallowfullscreen” not allowed on element “iframe” at this point."
-            },
-            {
-              text: "Attribute “webkitallowfullscreen” not allowed on element “iframe” at this point."
-            },
-            {
-              text: "Attribute “allowfullscreen” not allowed on element “iframe” at this point."
-            },
-            {
-              // Let <style> be in fragments.
-              text: "Element “style” not allowed as child of element “body” in this context. (Suppressing further errors from this subtree.)",
-              htmlFragment: true
-            },
-            {
-              // Let <li> be in fragments.
-              text: "Element “li” not allowed as child of element “body” in this context. (Suppressing further errors from this subtree.)",
-              htmlFragment: true
-            }
-          ]
-        }
-      }
     }
-
   });
 
-  // Actually load this plugin"s task(s).
-  grunt.loadTasks( "tasks" );
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks( "grunt-contrib-jshint" );
-  grunt.loadNpmTasks( "grunt-contrib-clean" );
-  grunt.loadNpmTasks( "grunt-contrib-nodeunit" );
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
   // By default, lint and run all tests.
-  grunt.registerTask( "default", [ "jshint", "html5_lint" ] );
+  grunt.registerTask('default', ['jshint']);
 
 };
